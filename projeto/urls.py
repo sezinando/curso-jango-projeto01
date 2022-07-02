838d910dee9d90f3e7784c9bab92200afe894b5e
Beto <robertosezinando@gmail.com>
2022-07-02 12:27:43 -0300

    Iniciando a inserção do container box recipe

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+from recipes.views import home
+
+def my_def(request):
+    return HttpResponse("<b>Uma Variável importante\
+    \n<t>Já retirando as informações da função presente</b>")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('local/', my_def),
+    path('', home),
 ]
